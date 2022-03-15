c13882aba2e20296a521f0b734c8b61a269bb1ce
Ex1Kx <thenewestekigoh@gmail.com>
2022-03-14 20:28:20 -0500

    [Ex1] Update The Project

---
+++ app/src/main/java/com/example/mathsolver/InfoMathSolver.kt
@@ -0,0 +1,11 @@
+package com.example.mathsolver
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class InfoMathSolver : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_info_math_solver)
+    }
+}
--- app/src/main/java/com/example/mathsolver/MainActivity.kt
+++ app/src/main/java/com/example/mathsolver/MainActivity.kt
@@ -1,11 +1,41 @@
 package com.example.mathsolver
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val first=findViewById<EditText>(R.id.editTextTextPersonName3)
+        val second=findViewById<EditText>(R.id.editTextTextPersonName4)
+        val add=findViewById<Button>(R.id.button6)
+        val sub=findViewById<Button>(R.id.button7)
+        val mult=findViewById<Button>(R.id.button8)
+        val div=findViewById<Button>(R.id.button9)
+        val result=findViewById<TextView>(R.id.textView2)
+        val info=findViewById<Button>(R.id.button10)
+
+        add.setOnClickListener {
+            result.setText("The Add Is: "+(first.text.toString().toInt()+second.text.toString().toInt()))
+        }
+        sub.setOnClickListener {
+            result.setText("The Sub Is: "+(first.text.toString().toInt()-second.text.toString().toInt()))
+        }
+        mult.setOnClickListener {
+            result.setText("The Mult Is: "+(first.text.toString().toInt()*second.text.toString().toInt()))
+        }
+        div.setOnClickListener {
+            result.setText("The Div Is: "+(first.text.toString().toInt()/second.text.toString().toInt()))
+        }
+        info.setOnClickListener {
+            val intent:Intent = Intent(this, InfoMathSolver::class.java)
+            startActivity(intent)
+        }
     }
 }
